42d0b504a25f0bb5f62a13bac67f066322468593
Auke van Slooten <auke@muze.nl>
2023-10-19 13:48:22 +0200

    added headers test for request added copy test for request

--- test/request.mjs
+++ test/request.mjs
@@ -6,3 +6,21 @@ tap.test('start', t => {
 	t.equal(req.url, 'https://example.com/')
 	t.end()
 })
+
+tap.test('copy', t => {
+	let req = new Request('https://example.com')
+	let req2 = metro.request(req, {url:'?foo=bar'})
+	t.equal(req.url, 'https://example.com/')
+	t.equal(req2.url, 'https://example.com/?foo=bar')
+	t.end()
+})
+
+tap.test('headers', t => {
+	let req = metro.request('https://example.com', {
+		headers: {
+			'X-Foo': 'bar'
+		}
+	})
+	t.has(req.headers.get('x-foo'), 'bar')
+	t.end()
+})
