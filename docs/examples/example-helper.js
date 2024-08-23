8459d74d569f851483d9f5016cda85000743d8a3
Ben Peachey <ben@muze.nl>
2024-08-23 20:58:31 +0200

    Add logic for loading examples and showing their output.

---
+++ docs/examples/example-helper.js
@@ -0,0 +1,67 @@
+function createHTML(text) {
+  const elements = []
+  const range = document.createRange();
+
+  const fragment = range.createContextualFragment(text)
+
+  fragment.childNodes.forEach(element => {
+    console.info('element', element.nodeName, element)
+    elements.push(element)
+  })
+
+  return elements;
+}
+
+function createJS(text) {
+  const script = document.createElement('script')
+  script.type = 'module'
+
+  text = replaceGitHubToken('my-token', text)
+
+  script.innerHTML = text
+
+  return script
+}
+
+function htmlEncoded(subject) {
+  return subject.replace(/[\u00A0-\u9999<>&]/g, i => '&#' + i.charCodeAt(0) + ';')
+}
+
+function loadExample(fileType, text) {
+  let content
+
+  switch (fileType) {
+    case 'html':
+      content = createHTML(text)
+      break
+    case 'js':
+      content = [createJS(text)]
+      break
+    default:
+      throw new Error(`Unknown or Unsupported file type "${fileType}"`)
+  }
+
+  return content
+}
+
+function replaceGitHubToken(subject, text) {
+  if (text.includes(subject)) {
+    let token = ''
+
+    if ( ! document.location.search.includes('token=')) {
+      token = prompt(`
+                    This example requires a GitHub Personal Access Token (PAT) to work.
+
+                    Please call this example with your PAT added to the URL as "?token=GH-TOKEN" (replacing GH-TOKEN with your PAT)
+
+                    Or provide a PAT in the prompt below
+                `)
+    } else {
+      token = document.location.search.split('token=')[1]
+    }
+
+    text = text.replace(subject, token)
+  }
+
+  return text
+}
